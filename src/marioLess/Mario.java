22a5aa8bab2c990bd42dd442d48553e8c190752c
Mr. Orleond <mr.orleond@gmail.com>
2024-04-04 15:23:17 +0300

    Mario_less. Выполнены все требования задания.

--- src/marioLess/Mario.java
+++ src/marioLess/Mario.java
@@ -1,12 +1,29 @@
 package marioLess;
+
+import java.util.Scanner;
+
 /*
  * Задача из гарвардского курса. Ссылка:
  * https://cs50.harvard.edu/x/2024/psets/1/mario/less/
  */
 public class Mario {
     public static void main(String[] args) {
-        for (int i = 1; i <= 8; i++) {
-            for (int j = i; j < 8; j++) {
+        Scanner scanner = new Scanner(System.in);
+        int height=0;
+        String str;
+        do {
+            System.out.print("Введите высоту лестницы: ");
+            str = scanner.nextLine();
+            if (isNumber(str)) {
+                height = Integer.parseInt(str);
+            }
+        } while (height <= 0);
+
+        printLadder(height);
+    }
+    public static void printLadder(int height) {
+        for (int i = 1; i <= height; i++) {
+            for (int j = i; j < height; j++) {
                 System.out.print(" ");
             }
             for (int j = 0; j < i; j++) {
@@ -14,5 +31,14 @@ public class Mario {
             }
             System.out.println();
         }
+    }
+    public static boolean isNumber(String str) {
+        int checkNum;
+        for (int i = 0; i < str.length(); i++) {
+            checkNum = (int)str.charAt(i) - 48;
+            if (checkNum < 0 || checkNum > 9 )
+                return false;
+        }
+        return true;
     }
 }
